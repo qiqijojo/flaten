668877e2e3f7c8ccd83d1c3eb98dfc4f3a89ea57
wwwq77 <774948439@qq.com>
2016-05-27 18:29:34 +0800

    消除重复提交

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,16 +3,18 @@
 function double_to_one(collection) {
 	var arr = [];
 	var reArr = [];
+	if(typeof(collection[i]) == "Array"){
 	for(var i=0; i<collection[i]; i++){
-		if(typeof(collection[i]) == "Array"){
+		
 			for(var j=0;j<collection[j];j++){
 				arr.push(collection[i][j]);
 			}
 		}
+	}
 		else{
 			arr.push(collection[i]);
 		}
-	}
+	
 	arr = arr.toString().split(",");
 	for(var i=0; i<arr.length; i++){
 		if(reArr.indexOf(parseInt(arr[i])) == -1){

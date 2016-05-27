a7c2654529628bb3135b8d23d2a9ac87539cb6ff
wwwq77 <774948439@qq.com>
2016-05-27 20:07:21 +0800

    降维提交

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -2,9 +2,9 @@
 
 function double_to_one(collection) {
 	var arr = [];
-	for(var i=0; i<collection[i]; i++){
-		if(typeof collection[i] == "Array"){
-			for(var j=0; j<collection[j]; j++){
+	for(var i=0;i<collection.length;i++){
+		if(typeof(collection[i]) == "Array"){
+			for(var j=0; j<collection[i].length; j++){
 				arr.push(collection[i][j]);
 			}
 		}

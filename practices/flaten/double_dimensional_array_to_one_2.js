fc477108996c95549ab09bac041ad996526050aa
wwwq77 <774948439@qq.com>
2016-05-27 20:07:37 +0800

    消除重复提交

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,9 +3,9 @@
 function double_to_one(collection) {
 	var arr = [];
 	var reArr = [];
-	for(var i=0; i<collection[i]; i++){
-		if(typeof collection[i] == "Array"){
-			for(var j=0;j<collection[j];j++){
+	for(var i=0;i<collection.length;i++){
+		if(typeof(collection[i]) == "Array"){
+			for(var j=0; j<collection[i].length; j++){
 				arr.push(collection[i][j]);
 			}
 		}

87645212c21122e95848ee4c8487c96d9d8aafba
wwwq77 <774948439@qq.com>
2016-05-27 18:40:32 +0800

    降维提交

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -2,17 +2,16 @@
 
 function double_to_one(collection) {
 	var arr = [];
-	if(typeof(collection[i]) == "Array"){
 	for(var i=0; i<collection[i]; i++){
+		if(typeof collection[i] == "Array"){
 			for(var j=0; j<collection[j]; j++){
 				arr.push(collection[i][j]);
 			}
 		}
-	}
 		else{
 			arr.push(collection[i]);
 		}
-	
+	}
 	arr = arr.toString().split(",");
 	for(var i=0; i<arr.length; i++){
 		arr[i] = parseInt(arr[i]);
